2f2e84364649d0c3e22670b69574cacd7f82ed78
hongbofan <hongbofan@company.com>
2018-09-30 14:57:33 +0800

    init spring boot

---
+++ mindcenter/src/main/java/bofan/red/MindcenterApplication.java
@@ -0,0 +1,12 @@
+package bofan.red;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindcenterApplication.class, args);
+	}
+}
---
+++ usercenter/src/main/java/bofan/red/UsercenterApplication.java
@@ -0,0 +1,12 @@
+package bofan.red;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercenterApplication.class, args);
+	}
+}
